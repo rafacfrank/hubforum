14e9556aedb36101ced38b5b82975ea056f43e60
Rafael Frank <rafacfrank@gmail.com>
2024-07-16 19:52:22 -0300

    Teste e propriedades para produção

---
+++ hubforum/src/test/java/com/rafacfrank/hubforum/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafacfrank.hubforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
