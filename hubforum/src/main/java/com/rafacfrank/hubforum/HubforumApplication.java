e902f6188e6c47d8ebf8314f4da7b3b3a0c71c8d
Rafael Frank <rafacfrank@gmail.com>
2024-07-16 18:54:24 -0300

    COnfigurações

---
+++ hubforum/src/main/java/com/rafacfrank/hubforum/HubforumApplication.java
@@ -0,0 +1,13 @@
+package com.rafacfrank.hubforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubforumApplication.class, args);
+	}
+
+}
